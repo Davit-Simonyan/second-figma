77b8d6f168270f5c1dc82bc50f1f316e81ab4d67
Davit-Simonyan <davit.simonyan1993@gmail.com>
2022-03-30 17:53:12 +0400

    project with pictures

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('blue_button')
+const div = document.getElementById('button_div_2')
+
+
+div.style.display = 'none'
+
+button.addEventListener('click',() => {
+        div.style.display = 'flex' 
+    
+})
